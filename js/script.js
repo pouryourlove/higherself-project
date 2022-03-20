3f1263535a1a58e8d3d514e2ef285247b70eb8ca
pouryourlove <lqu134@naver.com>
2022-03-21 00:59:17 +0900

    testimonial javascript added

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// local testimonials
+
+const testimonials = [
+  {
+    id: 1,
+    name: "Gaelle",
+    img: "https://i.postimg.cc/qMyJzWmH/testimonial-2.jpg",
+    text: "I really recommend this site",
+  },
+
+  {
+    id: 2,
+    name: "David",
+    img: "https://i.postimg.cc/XJjG84z5/testimonial-3.jpg",
+    text: "I like youuuuuuuuuuu",
+  },
+  {
+    id: 3,
+    name: "hello",
+    img: "https://i.postimg.cc/YChCt8vJ/avatar-e278114ff56fefeca5b1d16823f204f4.jpg",
+    text: "love youuu",
+  },
+];
+
+// select items
+const img = document.getElementById("person-img");
+console.log(img);
+const text = document.getElementById("info");
+const name = document.getElementById("author");
+
+const leftBtn = document.querySelector(".btn--left");
+const rightBtn = document.querySelector(".btn--right");
+
+//set starting item
+
+let currentItem = 0;
+
+//load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson();
+});
+
+//show person based on item
+
+function showPerson() {
+  const item = testimonials[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  info.textContent = item.text;
+}
+
+//show next person
+
+rightBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > testimonials.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+//show previous person
+
+leftBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = testimonials.length - 1;
+  }
+  showPerson(currentItem);
+});
